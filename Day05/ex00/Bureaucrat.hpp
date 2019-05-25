a6e36e9f1a7a74b9eacef915f131c3f20c2711dd
Terry FINKEL <nfinkel@student.42.fr>
2019-05-25 20:51:01 +0200

    Day05 ex00

---
+++ Day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,36 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+
+class Bureaucrat {
+private:
+                        Bureaucrat( ) {};
+    int                 _grade;
+    std::string const   _name;
+
+public:
+                        Bureaucrat( std::string name, int grade ) throw( GradeTooHighException, GradeTooLowException );
+                        Bureaucrat( Bureaucrat const & );
+    virtual             ~Bureaucrat( ) {};
+    Bureaucrat          &operator=( Bureaucrat const & );
+
+    class GradeTooHighException : public std::exception {
+    public:
+        const char      *what( ) const throw( );
+    };
+
+    class GradeTooLowException : public std::exception {
+    public:
+        const char      *what( ) const throw( );
+    };
+
+    void                decGrade( ) throw( GradeTooLowException );
+    int                 getGrade( ) const;
+    std::string const   getName( ) const;
+    void                incGrade( ) throw( GradeTooHighException );
+};
+
+std::ostream            &operator<<(std::ostream &, Bureaucrat const &);
+
+#endif /* BUREAUCRAT_HPP */
